a12b24e1c6694c7fa876c98af48723c6a0a742c4
Vasiliy <Freeneos@icloud.com>
2021-10-07 13:19:31 +0300

    swift-tools-version:5.3 .macOS(.v11)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "HeimdallrSPM",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v11),
         .iOS(.v13)
     ],
     products: [
