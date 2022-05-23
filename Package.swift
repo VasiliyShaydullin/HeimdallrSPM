d3898beb46f54998380791d7bce4bfd58bf1a70a
Vasiliy <Freeneos@icloud.com>
2022-05-23 11:23:57 +0300

    Use Result in Swift standard library instead of from a dependency.

--- Package.swift
+++ Package.swift
@@ -15,18 +15,18 @@ let package = Package(
             name: "HeimdallrSPM",
             targets: ["HeimdallrSPM"]),
     ],
-    dependencies: [
+//    dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(
-            url: "https://github.com/antitypical/Result.git",
-            from: "5.0.0"),
-    ],
+//        .package(
+//            url: "https://github.com/antitypical/Result.git",
+//            from: "5.0.0"),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HeimdallrSPM",
-            dependencies: ["Result"]),
+            dependencies: [/*"Result"*/]),
         .testTarget(
             name: "HeimdallrSPMTests",
             dependencies: ["HeimdallrSPM"]),
